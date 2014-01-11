60ed2410dc2c749caefc440e740ca98f5a861a51
pt <pt@pt-VirtualBox.(none)>
2014-01-11 11:47:19 +1100

    Image converter plugin with rudinentary unit tests, uses exiftool to extract metadata from image files in RDF format and make RDFa output. TODO: check rdfa is working to round-trip rdf data

--- potential-plugins/ImageConverterPlugin.py
+++ potential-plugins/ImageConverterPlugin.py
@@ -6,6 +6,8 @@ import logging
 import json 
 import Image
 import StringIO
+import rdflib
+import re
 
 class ImageConverterPlugin(HTMLFormatter):
     """ Image converter - requires Exiftool to be installed
@@ -27,16 +29,58 @@ class ImageConverterPlugin(HTMLFormatter):
         self.config = json.load(open("dispatcher-config.json"))
         self.previewSize = self.config["previewSize"]
         self.thumbnailSize = self.config["thumbnailSize"]
+        self.meta = dict()
+        self.defaultMetaOrder = ["Keywords",  "File Name",  "Directory",  "File Size",  "File Modification Date/Time",  "File Permissions",  "File Type",  "MIME Type",  "JFIF Version",  "Exif Byte Order",  "Image Description",  "Make",  "Camera Model Name",  "X Resolution",  "Y Resolution",  "Resolution Unit",  "Software",  "Modify Date",  "Artist",  "Y Cb Cr Positioning",  "Copyright",  "Exposure Time",  "F Number",  "Exposure Program",  "ISO",  "Sensitivity Type",  "Exif Version",  "Date/Time Original",  "Create Date",  "Components Configuration",  "Exposure Compensation",  "Max Aperture Value",  "Metering Mode",  "Light Source",  "Flash",  "Focal Length",  "Special Mode",  "Camera ID",  "User Comment",  "Flashpix Version",  "Color Space",  "Exif Image Width",  "Exif Image Height",  "Interoperability Index",  "Interoperability Version",  "Related Image Width",  "Related Image Height",  "File Source",  "Custom Rendered",  "Exposure Mode",  "White Balance",  "Digital Zoom Ratio",  "Scene Capture Type",  "Gain Control",  "Contrast",  "Saturation",  "Sharpness",  "Image Unique ID",  "Lens Info",  "Lens Model",  "PrintIM Version",  "Compression",  "Thumbnail Offset",  "Thumbnail Length",  "XMP Toolkit",  "Creator",  "Subject",  "Current IPTC Digest",  "Envelope Record Version",  "Coded Character Set",  "Application Record Version",  "Image Width",  "Image Height",  "Encoding Process",  "Bits Per Sample",  "Color Components",  "Y Cb Cr Sub Sampling",  "Aperture",  "Image Size",  "Shutter Speed",  "Thumbnail Image",  "Focal Length",  "Light Value"]
         
 
+    def rdfToDict(self, rdf):
+        """
+        Take an RDF graph from exiftool and flatten it into a dict keyed by label 
+        where meta[label] = (predicate,object)
+        All objects are Literals (strings)
+        """
+        g = rdflib.Graph()
+        g.parse(data=rdf)
+        subjectURI = rdflib.term.URIRef(u"http://ns.exiftool.ca/XMP/XMP-dc/1.0/Subject")
+        keywordsURI = rdflib.term.URIRef(u"http://ns.exiftool.ca/IPTC/IPTC/1.0/Keywords")
+        def unpackBag(term):
+            for s,o in g.subject_objects(term):
+                contentArray = []
+                for p1,o1 in g.predicate_objects(o):
+                   if type(o1) is rdflib.term.Literal:
+                        contentArray.append(o1)
+                #Get rid of statements about the bag / flatten
+                content = ", ".join(contentArray)
+                g.remove((o, None, None))
+                g.remove((None,None,o))
+                g.add((s,term,rdflib.term.Literal(content)))     
+        unpackBag(subjectURI)
+        unpackBag(keywordsURI)
+        
+        for s,p,o in g:
+            label = re.sub(".*/","",p)
+            label = re.sub("([A-Z][a-z])", " \\1", label)
+            self.meta[label[1:]] = (p, o)
+            
+    def dictToTable(self):
+        self.body = "<table>"
+        def formatRow(m):
+            (p,o) = self.meta.pop(m)
+            self.body += "<tr><td>%s</td><td property='%s'>%s</td></tr>" % (m,p,o)
+        for m in self.defaultMetaOrder:
+            if m in self.meta:
+                formatRow(m)
+                
+        #Spit out whats left
+        for m in self.meta.keys():
+            formatRow(m)
+            
+        self.body += "</table>"
         
 
     def convert(self, actableFile):
-        """Simple conversion script that runs markdown thru pandoc.
-        actableFile: ActionalableFile object from dispatcher.py
-        TODO: Fix relative image paths
-        TODO: Include RDFa info
-        TODO: Use json output from exiftool 
+        """
+           Extract metadata from image files and create an RDFa-ish index.html with a preview using a data URI
 
         """
             
@@ -45,8 +89,10 @@ class ImageConverterPlugin(HTMLFormatter):
         except:
             pass
        
-        body = subprocess.check_output(["exiftool", "-h", actableFile.path])
-        # TODO add to config
+        self.rdf = subprocess.check_output(["exiftool", "-X", actableFile.path])
+        self.rdfToDict(self.rdf)
+        self.dictToTable()
+        
         im = Image.open(actableFile.path)
         im.thumbnail(self.previewSize)
         
@@ -61,7 +107,7 @@ class ImageConverterPlugin(HTMLFormatter):
         actableFile.meta["dc:title"] = actableFile.filename
         actableFile.meta["thumbnail"] = thumbURI 
         #actableFile.meta["preview"] = prevURI #TOO big consider having a separate JSON file
-        html = "<html><title>%s</title><body><p><a href='../../%s'><img src='%s'></a></p>%s</body></html>" % (actableFile.meta["dc:title"],actableFile.filename, prevURI, body)
+        html = "<html><title>%s</title><body><p><a href='../../%s'><img src='%s'></a></p>%s</body></html>" % (actableFile.meta["dc:title"],actableFile.filename, prevURI, self.body)
         
         f = open(actableFile.indexHTML,"w")
         
---
+++ potential-plugins/test_ImageConverterPlugin.py
@@ -0,0 +1,155 @@
+from ImageConverterPlugin import ImageConverterPlugin
+import unittest
+
+
+class TestImageConverter(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_rdfToDict(self):
+        converter = ImageConverterPlugin()
+        converter.rdfToDict(rdfImage1)
+        (_,subject) = converter.meta["Subject"]
+        subjects = subject.split(", ")
+        expectedSubjects = ["People/Florence", "Vinyl"]
+        self.assertEqual(subjects.sort(),expectedSubjects.sort())
+        
+    def test_rdfToDict(self):
+        converter = ImageConverterPlugin()
+        converter.rdfToDict(rdfImage1)
+        converter.dictToTable()
+        print converter.body
+             
+        
+
+rdfImage1 = """<?xml version='1.0' encoding='UTF-8'?>
+<rdf:RDF xmlns:rdf='http://www.w3.org/1999/02/22-rdf-syntax-ns#'>
+
+<rdf:Description rdf:about='/home/pt/Documents/PC300326.JPG'
+  xmlns:et='http://ns.exiftool.ca/1.0/' et:toolkit='Image::ExifTool 8.60'
+  xmlns:ExifTool='http://ns.exiftool.ca/ExifTool/1.0/'
+  xmlns:System='http://ns.exiftool.ca/File/System/1.0/'
+  xmlns:File='http://ns.exiftool.ca/File/1.0/'
+  xmlns:JFIF='http://ns.exiftool.ca/JFIF/JFIF/1.0/'
+  xmlns:IFD0='http://ns.exiftool.ca/EXIF/IFD0/1.0/'
+  xmlns:ExifIFD='http://ns.exiftool.ca/EXIF/ExifIFD/1.0/'
+  xmlns:Olympus='http://ns.exiftool.ca/MakerNotes/Olympus/1.0/'
+  xmlns:InteropIFD='http://ns.exiftool.ca/EXIF/InteropIFD/1.0/'
+  xmlns:PrintIM='http://ns.exiftool.ca/PrintIM/PrintIM/1.0/'
+  xmlns:IFD1='http://ns.exiftool.ca/EXIF/IFD1/1.0/'
+  xmlns:XMP-x='http://ns.exiftool.ca/XMP/XMP-x/1.0/'
+  xmlns:XMP-xmp='http://ns.exiftool.ca/XMP/XMP-xmp/1.0/'
+  xmlns:XMP-dc='http://ns.exiftool.ca/XMP/XMP-dc/1.0/'
+  xmlns:IPTC='http://ns.exiftool.ca/IPTC/IPTC/1.0/'
+  xmlns:Photoshop='http://ns.exiftool.ca/Photoshop/Photoshop/1.0/'
+  xmlns:Composite='http://ns.exiftool.ca/Composite/1.0/'>
+ <ExifTool:ExifToolVersion>8.60</ExifTool:ExifToolVersion>
+ <System:FileName>PC300326.JPG</System:FileName>
+ <System:Directory>/home/pt/Documents</System:Directory>
+ <System:FileSize>463 kB</System:FileSize>
+ <System:FileModifyDate>2014:01:09 07:56:10+11:00</System:FileModifyDate>
+ <System:FilePermissions>rw-r--r--</System:FilePermissions>
+ <File:FileType>JPEG</File:FileType>
+ <File:MIMEType>image/jpeg</File:MIMEType>
+ <File:ExifByteOrder>Little-endian (Intel, II)</File:ExifByteOrder>
+ <File:CurrentIPTCDigest>4543d1375ecee103ceeb7db3770bf688</File:CurrentIPTCDigest>
+ <File:ImageWidth>768</File:ImageWidth>
+ <File:ImageHeight>1024</File:ImageHeight>
+ <File:EncodingProcess>Baseline DCT, Huffman coding</File:EncodingProcess>
+ <File:BitsPerSample>8</File:BitsPerSample>
+ <File:ColorComponents>3</File:ColorComponents>
+ <File:YCbCrSubSampling>YCbCr4:4:0 (1 2)</File:YCbCrSubSampling>
+ <JFIF:JFIFVersion>1.01</JFIF:JFIFVersion>
+ <JFIF:ResolutionUnit>None</JFIF:ResolutionUnit>
+ <JFIF:XResolution>1</JFIF:XResolution>
+ <JFIF:YResolution>1</JFIF:YResolution>
+ <IFD0:ImageDescription>OLYMPUS DIGITAL CAMERA         </IFD0:ImageDescription>
+ <IFD0:Make>OLYMPUS IMAGING CORP.</IFD0:Make>
+ <IFD0:Model>E-M5</IFD0:Model>
+ <IFD0:XResolution>350</IFD0:XResolution>
+ <IFD0:YResolution>350</IFD0:YResolution>
+ <IFD0:ResolutionUnit>inches</IFD0:ResolutionUnit>
+ <IFD0:Software>Version 1.6</IFD0:Software>
+ <IFD0:ModifyDate>2014:01:09 08:00:59</IFD0:ModifyDate>
+ <IFD0:Artist>Picasa</IFD0:Artist>
+ <IFD0:YCbCrPositioning>Co-sited</IFD0:YCbCrPositioning>
+ <IFD0:Copyright></IFD0:Copyright>
+ <ExifIFD:ExposureTime>1/80</ExifIFD:ExposureTime>
+ <ExifIFD:FNumber>10.0</ExifIFD:FNumber>
+ <ExifIFD:ExposureProgram>Aperture-priority AE</ExifIFD:ExposureProgram>
+ <ExifIFD:ISO>1000</ExifIFD:ISO>
+ <ExifIFD:SensitivityType>Standard Output Sensitivity</ExifIFD:SensitivityType>
+ <ExifIFD:ExifVersion>0230</ExifIFD:ExifVersion>
+ <ExifIFD:DateTimeOriginal>2013:12:30 16:31:54</ExifIFD:DateTimeOriginal>
+ <ExifIFD:CreateDate>2013:12:30 16:31:54</ExifIFD:CreateDate>
+ <ExifIFD:ComponentsConfiguration>Y, Cb, Cr, -</ExifIFD:ComponentsConfiguration>
+ <ExifIFD:ExposureCompensation>0</ExifIFD:ExposureCompensation>
+ <ExifIFD:MaxApertureValue>1.7</ExifIFD:MaxApertureValue>
+ <ExifIFD:MeteringMode>Multi-segment</ExifIFD:MeteringMode>
+ <ExifIFD:LightSource>Unknown</ExifIFD:LightSource>
+ <ExifIFD:Flash>Auto, Did not fire</ExifIFD:Flash>
+ <ExifIFD:FocalLength>20.0 mm</ExifIFD:FocalLength>
+ <ExifIFD:UserComment></ExifIFD:UserComment>
+ <ExifIFD:FlashpixVersion>0100</ExifIFD:FlashpixVersion>
+ <ExifIFD:ColorSpace>sRGB</ExifIFD:ColorSpace>
+ <ExifIFD:ExifImageWidth>768</ExifIFD:ExifImageWidth>
+ <ExifIFD:ExifImageHeight>1024</ExifIFD:ExifImageHeight>
+ <ExifIFD:FileSource>Digital Camera</ExifIFD:FileSource>
+ <ExifIFD:CustomRendered>Normal</ExifIFD:CustomRendered>
+ <ExifIFD:ExposureMode>Auto</ExifIFD:ExposureMode>
+ <ExifIFD:WhiteBalance>Auto</ExifIFD:WhiteBalance>
+ <ExifIFD:DigitalZoomRatio>1</ExifIFD:DigitalZoomRatio>
+ <ExifIFD:SceneCaptureType>Standard</ExifIFD:SceneCaptureType>
+ <ExifIFD:GainControl>High gain up</ExifIFD:GainControl>
+ <ExifIFD:Contrast>Normal</ExifIFD:Contrast>
+ <ExifIFD:Saturation>Normal</ExifIFD:Saturation>
+ <ExifIFD:Sharpness>Normal</ExifIFD:Sharpness>
+ <ExifIFD:ImageUniqueID>f32ac9ce66259d64321abe602559599d</ExifIFD:ImageUniqueID>
+ <ExifIFD:LensInfo>20mm f/1.7</ExifIFD:LensInfo>
+ <ExifIFD:LensModel>LUMIX G 20/F1.7</ExifIFD:LensModel>
+ <Olympus:SpecialMode>Normal, Sequence: 0, Panorama: (none)</Olympus:SpecialMode>
+ <Olympus:CameraID>OLYMPUS DIGITAL CAMERA         </Olympus:CameraID>
+ <InteropIFD:InteropIndex>R98 - DCF basic file (sRGB)</InteropIFD:InteropIndex>
+ <InteropIFD:InteropVersion>0100</InteropIFD:InteropVersion>
+ <InteropIFD:RelatedImageWidth>4608</InteropIFD:RelatedImageWidth>
+ <InteropIFD:RelatedImageHeight>3456</InteropIFD:RelatedImageHeight>
+ <PrintIM:PrintIMVersion>0300</PrintIM:PrintIMVersion>
+ <IFD1:Compression>JPEG (old-style)</IFD1:Compression>
+ <IFD1:XResolution>72</IFD1:XResolution>
+ <IFD1:YResolution>72</IFD1:YResolution>
+ <IFD1:ResolutionUnit>inches</IFD1:ResolutionUnit>
+ <IFD1:ThumbnailOffset>1910</IFD1:ThumbnailOffset>
+ <IFD1:ThumbnailLength>8310</IFD1:ThumbnailLength>
+ <XMP-x:XMPToolkit>XMP Core 5.1.2</XMP-x:XMPToolkit>
+ <XMP-xmp:ModifyDate>2014:01:09 08:00:59+11:00</XMP-xmp:ModifyDate>
+ <XMP-dc:Creator>Picasa</XMP-dc:Creator>
+ <XMP-dc:Subject>
+  <rdf:Bag>
+   <rdf:li>People/Florence</rdf:li>
+   <rdf:li>Vinyl</rdf:li>
+  </rdf:Bag>
+ </XMP-dc:Subject>
+ <IPTC:EnvelopeRecordVersion>4</IPTC:EnvelopeRecordVersion>
+ <IPTC:CodedCharacterSet>UTF8</IPTC:CodedCharacterSet>
+ <IPTC:ApplicationRecordVersion>4</IPTC:ApplicationRecordVersion>
+ <IPTC:Keywords>
+  <rdf:Bag>
+   <rdf:li>People/Florence</rdf:li>
+   <rdf:li>Vinyl</rdf:li>
+  </rdf:Bag>
+ </IPTC:Keywords>
+ <Photoshop:IPTCDigest>4543d1375ecee103ceeb7db3770bf688</Photoshop:IPTCDigest>
+ <Composite:Aperture>10.0</Composite:Aperture>
+ <Composite:ImageSize>768x1024</Composite:ImageSize>
+ <Composite:ShutterSpeed>1/80</Composite:ShutterSpeed>
+ <Composite:ThumbnailImage>(Binary data 8310 bytes, use -b option to extract)</Composite:ThumbnailImage>
+ <Composite:FocalLength35efl>20.0 mm</Composite:FocalLength35efl>
+ <Composite:LightValue>9.6</Composite:LightValue>
+</rdf:Description>
+</rdf:RDF>
+
+"""
+
+if __name__ == '__main__':
+    unittest.main()
